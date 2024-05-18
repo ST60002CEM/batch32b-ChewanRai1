97e991ae21685d6b65a626d00f2d758dfd34fb00
ChewanRai1 <220154@softwarica.edu.np>
2024-05-18 17:45:13 +0545

    connection btween mongodb and my final app

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:finalproject/app/app.dart';
 // import 'package:flutter_application_1/app/app..dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'mongodb.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await MongoDatabase.connect();
   runApp(
     const App(),
   );
---
+++ lib/url.dart
@@ -0,0 +1,3 @@
+const MONGO_URL =
+    "mongodb+srv://chewanrai:123qwe@handyhelperdb.yvmdh7v.mongodb.net/";
+const COLLECTION_NAME = "users";

31eb0c9ea299a9f403eae0e77f3043a4eacec0c1
ChewanRai1 <220154@softwarica.edu.np>
2024-07-20 12:12:59 +0545

    temprorary search view modified for screen feature

--- lib/features/dashboard/presentation/view/search_view.dart
+++ lib/features/dashboard/presentation/view/search_view.dart
@@ -1,19 +1,19 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class SearchView extends StatefulWidget {
-  const SearchView({super.key});
+// class SearchView extends StatefulWidget {
+//   const SearchView({super.key});
 
-  @override
-  State<SearchView> createState() => _SearchViewState();
-}
+//   @override
+//   State<SearchView> createState() => _SearchViewState();
+// }
 
-class _SearchViewState extends State<SearchView> {
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox.expand(
-      child: Center(
-        child: Text('You can search your helper here'),
-      ),
-    );
-  }
-}// presentation/view/search_view.dart (modified)
+// class _SearchViewState extends State<SearchView> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const SizedBox.expand(
+//       child: Center(
+//         child: Text('You can search your helper here'),
+//       ),
+//     );
+//   }
+// }// presentation/view/search_view.dart (modified)

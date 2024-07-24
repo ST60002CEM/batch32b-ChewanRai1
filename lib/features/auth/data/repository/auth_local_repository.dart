5c9b5fbdfb4ffcb9993e6fddd92124b271203b20
ChewanRai1 <220154@softwarica.edu.np>
2024-07-24 13:51:52 +0545

    data local repository modify/commented all for logout to work remotely

--- lib/features/auth/data/repository/auth_local_repository.dart
+++ lib/features/auth/data/repository/auth_local_repository.dart
@@ -1,36 +1,41 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:dartz/dartz.dart';
-import 'package:finalproject/core/failure/failure.dart';
-import 'package:finalproject/features/auth/data/data_source/local/auth_local_data_source.dart';
-import 'package:finalproject/features/auth/domain/entity/auth_entity.dart';
-import 'package:finalproject/features/auth/domain/repository/auth_repository.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:finalproject/core/failure/failure.dart';
+// import 'package:finalproject/features/auth/data/data_source/local/auth_local_data_source.dart';
+// import 'package:finalproject/features/auth/domain/entity/auth_entity.dart';
+// import 'package:finalproject/features/auth/domain/repository/auth_repository.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-final authLocalRepositoryProvider = Provider<IAuthRepository>((ref) {
-  return AuthLocalRepository(
-    ref.read(authLocalDataSourceProvider),
-  );
-});
+// final authLocalRepositoryProvider = Provider<IAuthRepository>((ref) {
+//   return AuthLocalRepository(
+//     ref.read(authLocalDataSourceProvider),
+//   );
+// });
 
-class AuthLocalRepository implements IAuthRepository {
-  final AuthLocalDataSource _authLocalDataSource;
+// class AuthLocalRepository implements IAuthRepository {
+//   final AuthLocalDataSource _authLocalDataSource;
 
-  AuthLocalRepository(this._authLocalDataSource);
+//   AuthLocalRepository(this._authLocalDataSource);
 
-  @override
-  Future<Either<Failure, bool>> loginUser(String email, String password) {
-    return _authLocalDataSource.loginUser(email, password);
-  }
+//   @override
+//   Future<Either<Failure, bool>> loginUser(String email, String password) {
+//     return _authLocalDataSource.loginUser(email, password);
+//   }
 
-  @override
-  Future<Either<Failure, bool>> registerUser(AuthEntity user) {
-    return _authLocalDataSource.registerUser(user);
-  }
+//   @override
+//   Future<Either<Failure, bool>> registerUser(AuthEntity user) {
+//     return _authLocalDataSource.registerUser(user);
+//   }
 
-  @override
-  Future<Either<Failure, String>> uploadProfilePicture(File file) async {
-    return const Right("");
-  }
-}
+//   @override
+//   Future<Either<Failure, String>> uploadProfilePicture(File file) async {
+//     return const Right("");
+//   }
+// }
+
+
+
+
+//commented because working on remotely and logout function

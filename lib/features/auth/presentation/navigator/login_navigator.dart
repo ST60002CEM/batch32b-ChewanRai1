6a759751e1e86486655ee2d53835b18ee4042c53
ChewanRai1 <220154@softwarica.edu.np>
2024-07-26 13:57:19 +0545

    navigation to the login view after logout

--- lib/features/auth/presentation/navigator/login_navigator.dart
+++ lib/features/auth/presentation/navigator/login_navigator.dart
@@ -1,16 +1,32 @@
+// import 'package:finalproject/app/navigator/navigator.dart';
+// import 'package:finalproject/features/auth/presentation/navigator/register_navigator.dart';
+// import 'package:finalproject/features/auth/presentation/view/login_view.dart';
+// import 'package:finalproject/features/dashboard/presentation/navigator/dashboard_navigator.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// // to go to the register view
+// final loginViewNavigatorProvider = Provider((ref) => LoginViewNavigator());
+
+// class LoginViewNavigator with RegisterViewRoute, DashboardViewRoute {}
+
+// mixin LoginViewRoute {
+//   openLoginView() {
+//     NavigateRoute.popAndPushRoute(const LoginView());
+//   }
+// }
+
 import 'package:finalproject/app/navigator/navigator.dart';
 import 'package:finalproject/features/auth/presentation/navigator/register_navigator.dart';
 import 'package:finalproject/features/auth/presentation/view/login_view.dart';
 import 'package:finalproject/features/dashboard/presentation/navigator/dashboard_navigator.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// to go to the register view
 final loginViewNavigatorProvider = Provider((ref) => LoginViewNavigator());
 
-class LoginViewNavigator with RegisterViewRoute, DashboardViewRoute {}
+class LoginViewNavigator with RegisterViewRoute, DashboardViewRoute, LoginViewRoute {}
 
 mixin LoginViewRoute {
-  openLoginView() {
+  void openLoginView() {
     NavigateRoute.popAndPushRoute(const LoginView());
   }
 }

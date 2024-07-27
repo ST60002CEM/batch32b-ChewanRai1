796f2643e74cf834de0d1326ba9708214ea51f4a
ChewanRai1 <220154@softwarica.edu.np>
2024-07-27 14:05:23 +0545

    navigator some comments added

--- lib/features/auth/presentation/navigator/login_navigator.dart
+++ lib/features/auth/presentation/navigator/login_navigator.dart
@@ -21,9 +21,11 @@ import 'package:finalproject/features/auth/presentation/view/login_view.dart';
 import 'package:finalproject/features/dashboard/presentation/navigator/dashboard_navigator.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// // to go to the register view
 final loginViewNavigatorProvider = Provider((ref) => LoginViewNavigator());
 
-class LoginViewNavigator with RegisterViewRoute, DashboardViewRoute, LoginViewRoute {}
+class LoginViewNavigator
+    with RegisterViewRoute, DashboardViewRoute, LoginViewRoute {}
 
 mixin LoginViewRoute {
   void openLoginView() {

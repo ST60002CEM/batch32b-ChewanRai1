eed8a263c5ee09ba74ddce42f8a960022e27ea41
ChewanRai1 <220154@softwarica.edu.np>
2024-08-14 07:15:01 +0545

    dashboard navigator modied

--- lib/features/dashboard/presentation/navigator/dashboard_navigator.dart
+++ lib/features/dashboard/presentation/navigator/dashboard_navigator.dart
@@ -1,7 +1,5 @@
 import 'package:finalproject/app/navigator/navigator.dart';
 import 'package:finalproject/features/dashboard/presentation/view/dashboard_view.dart';
-import 'package:finalproject/features/dashboard/presentation/view/home_view.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final dashboardViewNavigatorProvider =

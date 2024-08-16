45660071cbdfebd95de1e2bd3bc00eac62b19fad
ChewanRai1 <220154@softwarica.edu.np>
2024-08-17 01:33:02 +0545

    auth dto modify

--- lib/features/auth/data/dto/get_current_user_dto.dart
+++ lib/features/auth/data/dto/get_current_user_dto.dart
@@ -5,27 +5,27 @@ part 'get_current_user_dto.g.dart';
 
 @JsonSerializable()
 class GetCurrentUserDto {
-  @JsonKey(name:"_id")
+  @JsonKey(name: "_id")
   final String id;
   final String fname;
   final String lname;
   final String phone;
-  // final String image;
+  // final String? image; // Optional field
   final String username;
-  final String  email;
-  // final String batch;
-  // final List<String> course;
+  final String email;
+  // final String? batch; // Optional field
+  // final List<String>? course; // Optional field
 
   GetCurrentUserDto({
     required this.id,
     required this.fname,
     required this.lname,
     required this.phone,
-    // required this.image,
+    // this.image, // Optional field
     required this.username,
     required this.email,
-    // required this.batch,
-    // required this.course,
+    // this.batch, // Optional field
+    // this.course, // Optional field
   });
 
   AuthEntity toEntity() {
@@ -33,15 +33,17 @@ class GetCurrentUserDto {
         id: id,
         fname: fname,
         lname: lname,
-        // image: image,
+        // image: image ?? '', // Default value if null
         phone: phone,
-        // batch: BatchEntity(batchId: id, batchName: ''),
-        // courses:  course.map((course) {
-        //   return CourseEntity( courseId: course, courseName: '');
-        // }).toList() ,
         email: email,
         username: username,
-        password: '');
+        password: ''
+        // batch: batch ?? '', // Default value if null
+        // courses: course?.map((course) {
+        //       return CourseEntity(courseId: course, courseName: '');
+        //     }).toList() ??
+        //     [], // Default empty list if null
+        );
   }
 
   factory GetCurrentUserDto.fromJson(Map<String, dynamic> json) =>
